bc28fe50527473d1e7705a6279df6b4320b3298d
semoss <semoss@semosss-MacBook-Pro-2.local>
2019-08-22 10:25:52 -0400

    adding security and lm vol options

--- semoss/runConfig.sh
+++
@@ -1,9 +0,0 @@
-if [[ -z "${MONOLITH_COOKIE}" ]]; then
-sh setMonolithCookie.sh
-fi
-if [ "$SECURITY_ON" = "true" ]
-sh setSecurityOn.sh
-fi
-if [ "$SETSOCIAL" = "true" ]
-then sh setSocialProps.sh
-fi
